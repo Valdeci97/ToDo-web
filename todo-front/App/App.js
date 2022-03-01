9bcf0a2f0d72692c380787f35ddab43df457083e
Valdeci97 <moura97.valdeci@gmail.com>
2022-02-28 22:32:58 -0300

    build: Inicia projeto e organiza algumas pastas

---
+++ todo-front/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../src/components/Header';
+import * as S from './styles';
+
+function App() {
+  return (
+    <S.Container>
+      <Header />   
+    </S.Container>
+  );
+}
+
+export default App;
