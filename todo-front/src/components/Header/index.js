96f6a5b46adccc422902eed318eee3fedb3d4bd9
Valdeci97 <moura97.valdeci@gmail.com>
2022-03-06 17:02:21 -0300

    feat: Cria Header component

--- todo-front/src/components/Header/index.js
+++ todo-front/src/components/Header/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import * as S from './styles';
+import logo from '../../assets/logo-light.png';
+import bell from '../../assets/bell-light.png';
+
 export default function Header() {
   return (
-    <div>
-      Hello World.
-    </div>
+    <S.Container>
+      <S.LeftSide>
+        <img src={ logo } alt="Logo Todo" />
+      </S.LeftSide>
+      <S.RightSide>
+        <a href="#">INÍCIO</a>
+        <span className="pipe" />
+        <a href="#">NOVA TAREFA</a>
+        <span className="pipe" />
+        <a href="#">SINCRONIZAR CELULAR</a>
+        <span className="pipe" />
+        <a href="#" id="notification">
+          <img src={ bell } alt="Notificação" />
+          <span>5</span>
+        </a>
+      </S.RightSide>
+    </S.Container>
   );
 }
---
+++ todo-front/src/components/Header/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  background: #99AAEA;
+  display: flex;
+  border-bottom: 5px solid #25040c;
+`;
+
+export const LeftSide = styled.div`
+  width: 50%;
+  heigth: 70px;
+  display: flex;
+  align-items: center;
+  padding-left: 8px;
+
+  img {
+    height: 45px;
+    width: 100px;
+  }
+`;
+
+export const RightSide = styled.div`
+  width: 50%;
+  heigth: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  a {
+    text-decoration: unset;
+    color: #25040c;
+    font-weigth: bold;
+    margin: 0 10px;
+
+    &:hover {
+      color: #fafdec;
+    }
+  }
+
+  #notification {
+    img {
+      width: 40px;
+      heigth: 40px;
+    }
+
+    span {
+      background: #fafdec;
+      color: #25040c;
+      padding: 3px 6px;
+      border-radius: 50%;
+      position: relative;
+      top: -20px;
+      right: 20px;
+    }
+
+    &:hover {
+      opacity: 0.6;
+    }
+  }
+
+  .pipe::after {
+    color: #25040c;
+    content: "|"
+  }
+
+`;
